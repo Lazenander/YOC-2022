00bfebe82ae3683d2619a2c399e20d37f93f0b61
Haoran Yang <54178753+Lazenander@users.noreply.github.com>
2022-07-17 15:21:26 +0800

    Delete trash directory

--- trash/webcrawler.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-option = webdriver.ChromeOptions()
-#option.add_argument("headless")
-path = "./chromedriver"
-browser = webdriver.Chrome(path, options=option)
-
-browser.get('https://s.weibo.com/weibo?q=%E7%81%B5%E6%B4%BB%E7%94%A8%E5%B7%A5')
-browser.get_screenshot_as_file('截图.png')
